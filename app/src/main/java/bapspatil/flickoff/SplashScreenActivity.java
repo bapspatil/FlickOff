bca66122af24fd52e7b7f3d3cdb8a39ceaa2c512
Bapusaheb Patil <bapoo1998@gmail.com>
2017-10-27 19:24:37 +0530

    Made more design changes for Play Store release

--- app/src/main/java/bapspatil/flickoff/SplashScreenActivity.java
+++ app/src/main/java/bapspatil/flickoff/SplashScreenActivity.java
@@ -12,7 +12,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        int SPLASH_TIME_OUT = 3000;
+        int SPLASH_TIME_OUT = 1000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
