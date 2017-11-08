41bbb34327f61fa75f5c0bcbe0aa8daf65f42795
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-08 11:21:16 +0530

    Removed ads

--- app/src/main/java/bapspatil/flickoff/DetailsActivity.java
+++ app/src/main/java/bapspatil/flickoff/DetailsActivity.java
@@ -8,15 +8,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 public class DetailsActivity extends AppCompatActivity {
 
 
     private TextView mRatingTextView, mDateTextView, mTitleTextView, mPlotTextView;
     private ImageView mPosterImageView;
-    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +23,11 @@ public class DetailsActivity extends AppCompatActivity {
         toolbar.setLogo(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
 
-        mAdView = findViewById(R.id.adview);
         mRatingTextView = findViewById(R.id.rating_value_tv);
         mDateTextView = findViewById(R.id.date_value_tv);
         mTitleTextView = findViewById(R.id.title_tv);
         mPlotTextView = findViewById(R.id.plot_tv);
         mPosterImageView = findViewById(R.id.poster_image_view);
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
 
         Movie movie;
         Intent receivedIntent = getIntent();
@@ -46,29 +39,5 @@ public class DetailsActivity extends AppCompatActivity {
             mPlotTextView.setText(movie.getPlot());
             Glide.with(getApplicationContext()).load(movie.getPosterPath()).centerCrop().into(mPosterImageView);
         }
-    }
-
-    @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
     }
 }
